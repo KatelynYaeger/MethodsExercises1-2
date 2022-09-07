cddc777d43fbc115b4866649beac9f49b6f0e74d
Katelyn Yaeger <knyaeger93@gmail.com>
2022-09-07 17:15:02 -0500

    final commit method exercise

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using System.Drawing;
+using System.Reflection;
 
 namespace MethodExercise
 {
     class Program
+
+        ///------Exercise 2----------
+        
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
         static void Main(string[] args)
         {
-            //Exercise 1
+            var amountOfBooks = Sum(55, 45);
+            var amountOfCookies = Multiply(5, 6, 2);
+            var amountOfKittens = Subtract(12, 10);
+            var amountOfSteaks = Divide(25, 5);
+
+
+
+
+
+            //-----------------Exercise 1-------------
             //Your Name
             //Your Favorite Color
             //Your Favorite Animal
@@ -36,6 +71,10 @@ namespace MethodExercise
             Console.WriteLine($"Favorite color: {color}");
             Console.WriteLine($"Favorite animal: {animal}");
             Console.WriteLine($"Favorite band: {band}");
+
+            //exercise 2
+
+
         }
     }
 }
