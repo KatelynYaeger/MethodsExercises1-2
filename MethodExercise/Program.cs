4bf1b95fc3b1029792116176c0d5f04b8e9fa74b
Katelyn Yaeger <knyaeger93@gmail.com>
2022-09-06 19:19:26 -0500

    Exercise 1 initial commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1
+            //Your Name
+            //Your Favorite Color
+            //Your Favorite Animal
+            //Your Favorite Band
+
+            Console.WriteLine("Hello friend! What is your name?");
+
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}! What is your favorite color?");
+
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an amazing color! What is your favorite animal?");
+
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Another good choice! What about your favorite band?");
+
+            var band = Console.ReadLine();
+        }
+    }
+}
+
